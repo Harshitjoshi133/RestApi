39541cb5a17f7022a4764d089d7c1bb8c25f3792
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-09-14 06:39:05 +0530

    Adding GitIgnore

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-# from models.user_model import User
+from .models.user_model import User
 
 
 # Initialize Firebase Admin SDK
@@ -24,10 +24,6 @@ class LoginUser(BaseModel):
     username: str
     password: str
 
-
-@app.get("/")
-def hellyeah():
-    return {"messge":"hello"}
 
 @app.post("/register")
 async def register(user: RegisterUser):
